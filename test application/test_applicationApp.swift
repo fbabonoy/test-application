23b49b8ce241c3348c329fe31a92fe8eaa66c573
fernando <fernando.babonoyaba@gmail.com>
2021-09-22 09:03:00 -0600

    Initial Commit

---
+++ test application/test_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_applicationApp.swift
+//  test application
+//
+//  Created by fernando babonoyaba on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct test_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
